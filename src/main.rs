5ce06d9018150cc21a14a960d09a1374537f2a64
Dorian Lorek <dorian.lorek@etu.univ-lyon1.fr>
2022-04-28 12:16:53 +0200

    La méthode update crée un nouveau thread pour, dans le futur, sauvegarder l'entité dans un fichier

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use std::borrow::{Borrow, BorrowMut};
 use std::collections::HashMap;
-use std::ops::Deref;
-use std::rc::Rc;
+use std::thread;
+use std::thread::JoinHandle;
 
 trait IdEntity {
     fn get_id(&self) -> String;
     fn new(name: &str) -> Self;
 }
 
+#[derive(Clone)]
 struct Test {
     name: String,
     level: i16
@@ -16,13 +16,10 @@ struct Test {
 impl Test {
     fn set_level(&mut self, level: i16) {
         self.level = level;
-        self.save();
-    }
-
-    fn save(&self) {
-        //self.file_entity.save(self);
     }
 }
+
+unsafe impl Send for Test {}
 
 impl IdEntity for Test {
     fn get_id(&self) -> String {
@@ -37,30 +34,16 @@ impl IdEntity for Test {
     }
 }
 
-struct FileEntity<T: 'static> {
-    several: &'static SeveralFilesObject<T>
-}
-
-impl<T: IdEntity> FileEntity<T> {
-    fn new(several: &'static SeveralFilesObject<T>) -> FileEntity<T> {
-        FileEntity {
-            several
-        }
-    }
-
-    fn save(&self, entity: &T) {
-        self.several.update(entity);
-    }
-}
-
 struct SeveralFilesObject<T: 'static> {
-    id_to_file_entities: HashMap<String, T>
+    id_to_file_entities: HashMap<String, T>,
+    handles: Vec<JoinHandle<()>>
 }
 
-impl<T: IdEntity> SeveralFilesObject<T> {
+impl<T: IdEntity + Send + Clone> SeveralFilesObject<T> {
     fn new() -> SeveralFilesObject<T> {
         SeveralFilesObject {
-            id_to_file_entities: HashMap::new()
+            id_to_file_entities: HashMap::new(),
+            handles: Vec::new()
         }
     }
 
@@ -69,32 +52,51 @@ impl<T: IdEntity> SeveralFilesObject<T> {
         return self.add(entity);
     }
 
-    fn get_mut(&mut self, id: String) -> &mut T {
+    fn get_mut(&mut self, id: &str) -> &mut T {
         return self.id_to_file_entities.get_mut(&id.to_lowercase()).unwrap();
+    }
+
+    fn get(&self, id: &str) -> &T {
+        return self.id_to_file_entities.get(&id.to_lowercase()).unwrap();
     }
 
     fn add(&mut self, entity: T) -> String {
         let id = entity.get_id();
         self.id_to_file_entities.insert(id.to_lowercase(), entity);
-        let entity = self.id_to_file_entities.get(&id.to_lowercase()).unwrap();
-        self.update(entity);
+        self.update(&id);
         return id;
     }
 
-    fn update(&self, entity: &T) {
-        let id = entity.get_id().to_lowercase();
-        println!("Mise à jour de l'entité d'id {}", id);
-        // Mise à jour du fichier
+    fn update(&mut self, id: &str) {
+        let entity = self.id_to_file_entities.get(&id.to_lowercase());
+        match entity {
+            Some(entity) => {
+                // On doit clone l'entité pour pouvoir la move dans le nouveau thread sans que la map ne
+                // perde sa propriété
+                let entity = entity.clone();
+                let handle = thread::spawn(move || {
+                    println!("Mise à jour de l'entité d'id {}", entity.get_id());
+                });
+                self.handles.push(handle);
+            },
+            None => ()
+        }
     }
 }
 
 fn main() {
     println!("Hello, world!");
 
+
     let mut s: SeveralFilesObject<Test> = SeveralFilesObject::new();
     let id = s.create("Test2");
-    let f2 = s.get_mut(id);
-    f2.level = 5;
+    {
+        let f2 = s.get_mut(&id);
+        f2.set_level(5);
+        let id = f2.get_id();
+        s.update(&id);
+    }
+    let f2 = s.get(&id);
     println!("{} {}", f2.get_id(), f2.level);
 
     for (key, value) in s.id_to_file_entities.iter() {
@@ -114,4 +116,8 @@ fn main() {
     println!("Save");
     f.save();
     f2.save();*/
+
+    for handle in s.handles {
+        handle.join().unwrap();
+    }
 }
